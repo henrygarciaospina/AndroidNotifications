e81132e91911f0eb5bebadd42572319ea751eba1
HENRY GARCIA OSPINA <henrygarcia0309@gmail.com>
2021-08-31 19:56:42 -0500

    first commit

---
+++ app/src/main/java/com/arksunexus/androidnotifications/NotificationSettings.java
@@ -0,0 +1,6 @@
+package com.arksunexus.androidnotifications;
+
+public class NotificationSettings {
+    public static String HubName = "NotificationApi";
+    public static String HubListenConnectionString = "Endpoint=sb://comensalesapi.servicebus.windows.net/;SharedAccessKeyName=DefaultFullSharedAccessSignature;SharedAccessKey=w7HI7OSNRJiR3dhM+IOO+lMUqm17A4WeomKTuOQKAUY=";
+}
